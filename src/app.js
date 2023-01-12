c734b83f01dc9c9ab1c995d19b222db394238118
wlisses <wlissesfernando285@gmail.com>
2023-01-12 12:59:43 -0300

    verify

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ const peopleGet = require('./routes/PeopleGet')
 const peoplePost = require('./routes/peoplePost');
 const peoplePut = require('./routes/PeoplePut');
 const peopleDelete = require('./routes/PeopleDelete');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 app.use('/people', peopleGet);
 app.use('/people', peoplePost);
